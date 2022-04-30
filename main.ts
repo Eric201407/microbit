88cdb7acf97e7d153e86a5d6a70cf4b384f7023e
Eric201407 <104667971+Eric201407@users.noreply.github.com>
2022-04-30 13:43:34 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 0)
+})
+basic.forever(function () {
+	
+})
