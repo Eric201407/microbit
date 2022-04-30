40b3959e501653261ea2a34f60ed10a6c0c7ca9a
Eric201407 <104667971+Eric201407@users.noreply.github.com>
2022-04-30 13:49:17 +0800

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-basic.forever(function () {
-	
-})
+def on_button_pressed_a():
+    pins.analog_write_pin(AnalogPin.P0, 1023)
+    pins.analog_write_pin(AnalogPin.P1, 0)
+    pins.analog_write_pin(AnalogPin.P2, 0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    pins.analog_write_pin(AnalogPin.P0, 0)
+    pins.analog_write_pin(AnalogPin.P1, 0)
+    pins.analog_write_pin(AnalogPin.P2, 1023)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    pins.analog_write_pin(AnalogPin.P0, 0)
+    pins.analog_write_pin(AnalogPin.P1, 1023)
+    pins.analog_write_pin(AnalogPin.P2, 0)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     pins.analogWritePin(AnalogPin.P0, 1023)
     pins.analogWritePin(AnalogPin.P1, 0)
     pins.analogWritePin(AnalogPin.P2, 0)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     pins.analogWritePin(AnalogPin.P0, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
     pins.analogWritePin(AnalogPin.P2, 1023)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     pins.analogWritePin(AnalogPin.P0, 0)
     pins.analogWritePin(AnalogPin.P1, 1023)
     pins.analogWritePin(AnalogPin.P2, 0)
 })
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
