694cb8f3623a4b36d5bd9ceebc272eb9eddf1fd2
Eric201407 <104667971+Eric201407@users.noreply.github.com>
2022-04-30 14:08:30 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     pins.analogWritePin(AnalogPin.P0, 1023)
     pins.analogWritePin(AnalogPin.P1, 0)
     pins.analogWritePin(AnalogPin.P2, 0)
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     pins.analogWritePin(AnalogPin.P0, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
     pins.analogWritePin(AnalogPin.P2, 1023)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     pins.analogWritePin(AnalogPin.P0, 0)
     pins.analogWritePin(AnalogPin.P1, 1023)
     pins.analogWritePin(AnalogPin.P2, 0)
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
+	
 })
